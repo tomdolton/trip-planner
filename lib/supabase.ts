70e89d8de570cf32a6e0a898ef56042f17c9a482
tomdolton <trmdolton@gmail.com>
2025-07-07 18:44:30 +0100

    Fix: add error catch to supabase instatiation file.

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,5 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
+if (!process.env.NEXT_PUBLIC_SUPABASE_URL || !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
+  throw new Error('Supabase environment variables are not set');
+}
+
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
